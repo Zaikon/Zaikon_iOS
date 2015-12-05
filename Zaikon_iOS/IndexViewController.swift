a41173fb2e347706ea0e9c55476ef9a4003a095e
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-05 13:29:47 +0900

    set navigation bar

--- Zaikon_iOS/IndexViewController.swift
+++ Zaikon_iOS/IndexViewController.swift
@@ -13,6 +13,16 @@ class IndexViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.title = "Zaikon"
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        let lightBlue = UIColor(red: 75/256, green: 199/256, blue: 241/256, alpha: 1)
+        self.navigationController!.navigationBar.barTintColor = lightBlue
+        self.navigationItem.title = "Zaikon"
+//        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: UIFont(name: "Chalkduster", size: 30)!]        
     }
 
     override func didReceiveMemoryWarning() {
