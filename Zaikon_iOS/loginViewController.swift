63a83f82cf047f5fbe777cd357c1d7011e5006d7
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-09 14:52:18 +0900

    install icon and set restrict in interface builder

---
+++ Zaikon_iOS/loginViewController.swift
@@ -0,0 +1,23 @@
+//
+//  loginViewController.swift
+//  Zaikon_iOS
+//
+//  Created by 三浦立輝 on 2015/12/09.
+//  Copyright © 2015年 Tatsuki_Miura. All rights reserved.
+//
+
+import UIKit
+
+class loginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
