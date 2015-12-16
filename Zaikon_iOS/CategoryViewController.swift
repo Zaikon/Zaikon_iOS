5db2c0f2c90248a41b870604ff8612631e3f65cd
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-16 15:29:23 +0900

    design menu bar

--- Zaikon_iOS/CategoryViewController.swift
+++ Zaikon_iOS/CategoryViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class CategoryViewController: UIViewController {
 
+    let lightBlue = UIColor(red: 75/255, green: 199/255, blue: 241/255, alpha: 1)
+    let lightGrey = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
     var pageMenu : CAPSPageMenu?
 
     override func viewDidLoad() {
@@ -17,37 +19,40 @@ class CategoryViewController: UIViewController {
         
         // app title
         self.title = "ザイコン"
-        
-        // set initial methods
-        self.view.backgroundColor = UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
+        let navBarHeight = self.navigationController?.navigationBar.frame.size.height
+        let statusBarHeight: CGFloat = UIApplication.sharedApplication().statusBarFrame.height
         
         var controllerArray : [UIViewController] = []
         
         let controller1 :GoodsViewController = GoodsViewController(nibName: "GoodsViewController", bundle: nil)
-        controller1.title = "FOODS"
+        controller1.title = "ドリンク"
         controllerArray.append(controller1)
         
         let controller2 :GoodsViewController = GoodsViewController(nibName: "GoodsViewController", bundle: nil)
-        controller2.title = "DRINK"
+        controller2.title = "食べ物"
         controllerArray.append(controller2)
         
         let controller3 :GoodsViewController = GoodsViewController(nibName: "GoodsViewController", bundle: nil)
-        controller3.title = "GROCERY"
+        controller3.title = "その他"
         controllerArray.append(controller3)
         
-        
+        let arrayNum = controllerArray.count
+        let menuWidth = self.view.frame.width / CGFloat(arrayNum)
         let parameters: [CAPSPageMenuOption] = [
-            .ScrollMenuBackgroundColor(UIColor(red: 30.0/255.0, green: 30.0/255.0, blue: 30.0/255.0, alpha: 1.0)),
-            .ViewBackgroundColor(UIColor(red: 20.0/255.0, green: 20.0/255.0, blue: 20.0/255.0, alpha: 1.0)),
-            .SelectionIndicatorColor(UIColor.orangeColor()),
-            .BottomMenuHairlineColor(UIColor(red: 70.0/255.0, green: 70.0/255.0, blue: 80.0/255.0, alpha: 1.0)),
-            .MenuItemFont(UIFont(name: "HelveticaNeue", size: 13.0)!),
-            .MenuHeight(80.0),
-            .MenuItemWidth(90.0),
-            .CenterMenuItems(true)
+            .ScrollMenuBackgroundColor(UIColor.whiteColor()),
+            .SelectedMenuItemLabelColor(lightBlue),
+            .ViewBackgroundColor(lightGrey),
+            .SelectionIndicatorColor(lightBlue),
+            .SelectedMenuItemLabelColor(lightBlue),
+            .UnselectedMenuItemLabelColor(UIColor.blackColor()),
+            .MenuItemFont(UIFont(name: "Chalkduster", size: 13.0)!),
+            .MenuHeight(30.0),
+            .MenuItemWidth(menuWidth),
+            .MenuMargin(0.0),
+            .CenterMenuItems(true),
         ]
         
-        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, 0.0, self.view.frame.width, self.view.frame.height), pageMenuOptions: parameters)
+        pageMenu = CAPSPageMenu(viewControllers: controllerArray, frame: CGRectMake(0.0, CGFloat(navBarHeight!) + statusBarHeight, self.view.frame.width, self.view.frame.height), pageMenuOptions: parameters)
         
         
         self.addChildViewController(pageMenu!)
@@ -58,7 +63,6 @@ class CategoryViewController: UIViewController {
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
-        let lightBlue = UIColor(red: 75/255, green: 199/255, blue: 241/255, alpha: 1)
         self.navigationController!.navigationBar.barTintColor = lightBlue
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor(), NSFontAttributeName: UIFont(name: "Chalkduster", size: 25)!]
     }
