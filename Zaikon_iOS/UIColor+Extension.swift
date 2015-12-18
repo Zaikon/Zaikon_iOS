dcd9d6aa687e6dd6908b80d5bae6f905e84a390f
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-18 14:27:54 +0900

    define extra class method to make border in collection View cell

--- Zaikon_iOS/UIColor+Extension.swift
+++ Zaikon_iOS/UIColor+Extension.swift
@@ -15,6 +15,10 @@ extension UIColor {
         return UIColor(red: 248/255, green: 248/255, blue: 248/255, alpha: 1)
     }
     
+    class func Grey() -> UIColor {
+        return UIColor(red: 241/255, green: 241/255, blue: 241/255, alpha: 1)
+    }
+    
     class func lightBlue() -> UIColor {
         return UIColor(red: 75/255, green: 199/255, blue: 241/255, alpha: 1)
     }
